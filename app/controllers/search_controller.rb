129ec60bf097a8664fe811795765f36f698cbe2d
jayphodges <jayphodges@gmail.com>
2017-10-11 10:46:33 -0600

    Refactored search controller to remove faraday logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @members = MemberSearch.all_by_state(params[:state]).members
-
-
-
-    state = params[:state]
-    @conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-KEY"] = ENV["PROPUBLICA_API_KEY"]
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = @conn.get("/congress/v1/members/house/#{state}/current.json")
-
-    @members = JSON.parse(response.body, symbolize_names: true)[:results].map do |raw_member|
-      Member.new(raw_member)
-    end
+    @members = MemberSearch.new(params[:state]).members
   end
+
+
+
 end
--- app/services/member_search.rb
+++ app/services/member_search.rb
@@ -1,10 +1,27 @@
 class MemberSearch
 
   def initialize(us_state)
+    @us_state = us_state
   end
 
-  def all_by_state
+  def members
+    # @members = raw_members.each do |raw_member|
+    #   Member.new(raw_member)
+    # end
 
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV["PROPUBLICA_API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/congress/v1/members/house/#{us_state}/current.json")
+
+    @members = JSON.parse(response.body, symbolize_names: true)[:results].map do |raw_member|
+      Member.new(raw_member)
+    end
   end
+
+  private
+    attr_reader :us_state
 
 end
