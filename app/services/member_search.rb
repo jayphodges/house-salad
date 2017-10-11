70f55c46dc06bc0bb4f332ea58863e8b29b831aa
jayphodges <jayphodges@gmail.com>
2017-10-11 10:11:59 -0600

    Added MemberSearch testing

---
+++ app/services/member_search.rb
@@ -0,0 +1,10 @@
+class MemberSearch
+
+  def initialize(us_state)
+  end
+
+  def all_by_state
+
+  end
+
+end
---
+++ spec/services/member_search_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe MemberSearch do
+  context "class methods" do
+    context ".initialize(us_state)" do
+      it "returns a valid object" do
+        search = MemberSearch.new('CO')
+
+        expect(search).to be_a MemberSearch
+      end
+    end
+  end
+  context "intance methods" do
+    let(:search) { MemberSearch.new('CO') }
+
+    context "#members" do
+      it "returns a lit of members with a valid state" do
+        expect(search.members.count).to eq(7)
+      end
+
+    end
+  end
+end
